2a5baf6c9542a195d62752f7ca98bd77eb477a31
KhudaykovaAleksa <KhudaykovaAleksa@users.noreply.github.com>
2020-11-26 20:20:06 +0500

    save

---
+++ js/createTable.js
@@ -0,0 +1,8 @@
+let numberRows = document.detElementById("#numberRows");
+let numberColumns = document.detElementById("#numberColumns");
+
+tableBorderWidth = document.getElementById("tableBorderWidth")+"px";
+tableBorderType = document.querySelector(".table-border-type").value;
+
+alert(tableBorderWidth);
+
